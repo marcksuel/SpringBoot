ef3be91e34d82a2d3aca777ef4706d027e93cef3
marcksuel <marcksuel.menezes@gmail.com>
2023-03-13 17:36:16 -0300

    CRUD - Update, User

--- course/course/src/main/java/com/marcksuel/course/services/UserService.java
+++ course/course/src/main/java/com/marcksuel/course/services/UserService.java
@@ -28,7 +28,20 @@ public class UserService {
 	public User insert(User user) {
 		return repository.save(user);
 	}
+	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id,User user) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, user);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
 	}
 }
