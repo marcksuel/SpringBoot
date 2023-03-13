b733cc1b545dcf6dbf7ac3b684d8333c5554bf20
marcksuel <marcksuel.menezes@gmail.com>
2023-03-13 00:18:57 -0300

    Service layer, component registration

--- course/course/src/main/java/com/marcksuel/course/repositories/UserRepository.java
+++ course/course/src/main/java/com/marcksuel/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marcksuel.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
