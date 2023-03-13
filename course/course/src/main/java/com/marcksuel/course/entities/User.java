d012c8a6feb5b5fb709d950fd10530e9f7368ee7
marcksuel <marcksuel.menezes@gmail.com>
2023-03-12 22:07:36 -0300

    User entity and resource

---
+++ course/course/src/main/java/com/marcksuel/course/entities/User.java
@@ -0,0 +1,89 @@
+package com.marcksuel.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "User \n" + "id: " + id + "" + "\nname: " + name + "\nemail: " + email + "\nphone: " + phone
+				+ "\npassword:" + password;
+	}
+
+}
---
+++ course/course/src/main/java/com/marcksuel/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.marcksuel.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcksuel.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Max","max@gmail.com","8399991111","123456789");
+		return ResponseEntity.ok().body(u);
+	}
+}
