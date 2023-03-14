349f9ef46c443c34d9fb797d6b2a57a0c0ef57f0
marcksuel <marcksuel.menezes@gmail.com>
2023-03-14 13:21:11 -0300

    UPDATE - EntityNotFoundException

--- course/course/src/main/java/com/marcksuel/course/services/UserService.java
+++ course/course/src/main/java/com/marcksuel/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.marcksuel.course.repositories.UserRepository;
 import com.marcksuel.course.services.exceptions.DatabaseException;
 import com.marcksuel.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,14 @@ public class UserService {
 		}
 	}
 	public User update(Long id,User user) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, user);
-		return repository.save(entity);
+		try{
+			User entity = repository.getReferenceById(id);
+			updateData(entity, user);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User user) {
